76c80311e4e832bc30d87aff5b231b3612333e55
Ideawizard <ryan@ryansvideogames.com>
2020-02-13 16:32:40 -0500

    I gave the FileReader the ability to read multiple lines, all of which are null terminated

--- lab1/FileReader.c
+++ lab1/FileReader.c
@@ -1,27 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+
+char* ReadLine(FILE *file, char output[]);
 
 int main(int argc, char** argv){
 
-	FILE *file;
+	FILE *file;	
+	char* input = argv[1];
 	char output[1000];
-
+	char* fileText[100];
 	
-	file = fopen(argv[1], "r");
+	file = fopen(input, "r");
 
 	if (file == NULL)
 	{
-		printf("Could not open file %s", argv[1]);
+		printf("Could not open file %s", input);
 		return 1;
 	}
 
-	if (fgets(output, 1000, file) != NULL)
-	{	output[999] = "\0";
-		printf("%s", output);
+	else
+	{	for(int i = 0; ReadLine(file, output) != NULL ;i++)
+		{
+			fileText[i] = ReadLine(file, output);
+		}
 	}
-	fclose(file);
+
 	return 0;
 
 
 }
 
+char* ReadLine(FILE *file, char output[])
+{
 
+	if (fgets(output, 1000, file) != NULL)
+	{	output[999] = '\0';
+		printf("%s", output);
+	
+	}
+	else
+	{
+		output = NULL;
+		fclose(file);
+	}
+
+	return output;
+}
